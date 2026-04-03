fd4458945cacdef0b0c33eca4700cfa8a806faad
OkemaPaulMark <paulokema342@gmail.com>
2026-04-03 11:33:10 +0300

    frontend changes

--- frontend/src/services/productService.js
+++ frontend/src/services/productService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const api = axios.create({ baseURL: "/api" });
+const api = axios.create({ baseURL: `${import.meta.env.VITE_API_URL}/api` });
 
 export const getProducts = () => api.get("/products");
 export const getProduct = (id) => api.get(`/products/${id}`);
